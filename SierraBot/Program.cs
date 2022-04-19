20cf6f2b854e3ae7a1e8cff0c7edf21ace1cbc83
Shadow <shadic@msn.com>
2022-04-18 21:46:54 -0400

    Initial Commit

---
+++ SierraBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace SierraBot
+{
+	public class Program
+	{
+		private DiscordSocketClient _client;
+
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+		}
+	}
+}
